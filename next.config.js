14342912f541b59b7e77ee8ba10cac7337bf5300
jdorber94 <jdorber94@gmail.com>
2025-03-06 23:09:26 +0100

    Fix Next.js configuration: remove TS config and enhance JS config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   poweredByHeader: false,
+  images: {
+    domains: [],
+    remotePatterns: [],
+  },
+  experimental: {
+    optimizeCss: true,
+    scrollRestoration: true,
+  },
 };
 
 module.exports = nextConfig; 
